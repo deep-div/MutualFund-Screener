9eec28c3fab3eda38904045f3f2dae6607455663
deep-div <div20023041@gmail.com>
2026-03-10 21:32:18 +0530

    added GZipMiddleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
 
 from app.api.router import router as api_router
 from app.db.session import warm_up_db
@@ -17,6 +18,7 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+app.add_middleware(GZipMiddleware, minimum_size=1024)
 app.include_router(api_router, prefix="/api")
 
 # py -m uvicorn app.main:app --reload --host 127.0.0.1 --port 8000
