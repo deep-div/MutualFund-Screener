bc1d005639fdac0ce061155d326353151f9096a6
deep-div <div20023041@gmail.com>
2026-03-11 21:55:47 +0530

    refactor

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import mf, users
+from app.api.v1.endpoints import users
+from backend.app.api.v1.endpoints import mutual_fund
 
 router = APIRouter()
 
-router.include_router(mf.router, tags=["Mutual Funds"])
+router.include_router(mutual_fund.router, tags=["Mutual Funds"])
 router.include_router(users.router, tags=["Users"])
