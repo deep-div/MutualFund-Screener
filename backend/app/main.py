76612d9b1dc31a29f6e1604c111d9dab4a07c2b1
deep-div <div20023041@gmail.com>
2026-03-12 14:16:27 +0530

    logger in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,12 +4,14 @@ from fastapi.middleware.gzip import GZipMiddleware
 
 from app.api.v1.router import router as api_router
 from app.db.session import init_db
-
+from app.core.logging import logger
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """Handles startup and shutdown events for the FastAPI app"""
+    logger.info("Starting DB Connections")
     init_db()
+    logger.info("Shutting down DB Connections")
     yield
 
 
