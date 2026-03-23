697dae36561e2af76257e60d1e711e06ce61d8e8
deep-div <div20023041@gmail.com>
2026-03-23 17:25:33 +0530

    userservices

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,5 +1,5 @@
 import { apiPost } from "@/lib/apiClient";
 
-export const syncUser = async (firebaseToken: string) => {
-  return apiPost("/api/v1/users", undefined, { params: { firebase_token: firebaseToken } });
+export const syncUser = async (token: string) => {
+  return apiPost("/api/v1/users", undefined, { params: { token: token } });
 };
