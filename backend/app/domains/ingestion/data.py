f95d04c7f1744f97d8ccb223d37694a3f9863b59
deep-div <div20023041@gmail.com>
2026-03-28 15:36:46 +0530

    # https://www.amfiindia.com/api/nav-history?query_type=historical_period&from_date=2021-03-27&to_date=2026-03-25&sd_id=119551

--- backend/app/domains/ingestion/data.py
+++ backend/app/domains/ingestion/data.py
@@ -8,3 +8,6 @@
 # For Historical Data
 # https://api.mfapi.in/mf/128627  ## Scheme Code    
 # https://mf.captnemo.in/nav/INF740K01ZW2  ## ISIN Code
+
+## Can add 
+# https://www.amfiindia.com/api/nav-history?query_type=historical_period&from_date=2021-03-27&to_date=2026-03-25&sd_id=119551
