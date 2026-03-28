a98d62907050ef355fa4d72acf537f682aef46d3
deep-div <div20023041@gmail.com>
2026-03-28 13:36:59 +0530

    data.py to remember api used

---
+++ backend/app/domains/ingestion/data.py
@@ -0,0 +1,10 @@
+# For Latest Date Data
+# https://api.mfapi.in/mf/latest
+# https://portal.amfiindia.com/spages/NAVAll.txt
+
+
+
+
+# For Historical Data
+# https://api.mfapi.in/mf/128627  ## Scheme Code    
+# https://mf.captnemo.in/nav/INF740K01ZW2  ## ISIN Code
